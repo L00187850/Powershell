1e5e6c651095bd35271c489bf90cc9078c6741d0
unknown <L00187850@atu.ie>
2024-12-05 20:43:48 +0000

    Got to step 6

---
+++ 3. Install PowerShell7.ps1
@@ -0,0 +1,7 @@
+$MYPARAMS = @{
+	UseMSI = $true
+	Quiet = $true
+	AddExplorerContextMenu = $true
+	EnablePSRemoting = $true
+}
+C:\PowerShell\Install-Powershell.ps1 @MYPARAMS
---
+++ 6. FindModulesPSCore.ps1
@@ -0,0 +1,2 @@
+$PGSM = Find-Module -Name * -Tag 'PSEdition-Core'
+"There are {0:N0} Modules that support Powershell Core" -f $PGSM.count
